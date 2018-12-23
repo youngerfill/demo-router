5cee4c9ac03d580ec2e916028543f234df3f1c9a
bergoid <bergoid@users.noreply.github.com>
2018-12-24 00:48:27 +0100

    no homepage

---
+++ docs/precache-manifest.44a71bf450c6fd6681681029e9b40e7e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "60a7b95b73775648f667",
+    "url": "/static/js/main.60a7b95b.chunk.js"
+  },
+  {
+    "revision": "4028f4a9611ec0d1c01b",
+    "url": "/static/js/1.4028f4a9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7e3dfab6d3209e6f04091db31de5aa36",
+    "url": "/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/demo-router/precache-manifest.2260ecc4b3c6abc1817afdd55b55e2e1.js"
+  "/precache-manifest.44a71bf450c6fd6681681029e9b40e7e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/demo-router/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
