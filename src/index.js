b3d1e31e3562be45bb030471647db899f4dbfbc0
bergoid <bergoid@users.noreply.github.com>
2018-12-23 23:11:10 +0100

    First commit

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import * as h from 'react-hyperscript-helpers';
+
+const Main = () => { return h.div("[Main]") }
+const Foo = () => { return h.div("[Foo]") }
+const Bar = () => { return h.div("[Bar]") }
+
+const Page = () =>
+{
+    return h.h(
+        Switch,
+        [
+            h.h(
+                Route,
+                {
+                    exact: true,
+                    path: '/',
+                    component: Main
+                }
+            ),
+            h.h(
+                Route,
+                {
+                    path: '/foo',
+                    component: Foo
+                }
+            ),
+            h.h(
+                Route,
+                {
+                    path: '/bar',
+                    component: Bar
+                }
+            )
+        ]
+    )
+}
+
+ReactDOM.render(
+    h.h(
+        BrowserRouter,
+        [
+            h.h(Page)
+        ]
+    ),
+    document.getElementById('root')
+);
+
